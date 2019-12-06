455d3e9e815dfe4d4e928d9a046b07755f6fd407
souvikdas2411 <57383567+souvikdas2411@users.noreply.github.com>
2019-12-07 00:02:37 +0530

    Add files via upload

---
+++ create_data.py
@@ -0,0 +1,30 @@
+import cv2, sys, numpy, os 
+haar_file = 'cascade.xml'	# FRONTAL FACE+MY DATA+AMITABH'S DATA
+datasets = 'datasets'
+sub_data = input("Enter Name : ")
+# if sub_data=="quit":
+# 	break	
+path = os.path.join(datasets, sub_data) 
+if not os.path.isdir(path): 
+	os.mkdir(path) 
+(width, height) = (130, 100)	 
+face_cascade = cv2.CascadeClassifier(haar_file) 
+webcam = cv2.VideoCapture(0) #1 for any other cam
+#cam = cv2.VideoCapture('http://192.168.43.7:8080/video')
+
+count = 0
+while count >=0: 
+	(_, im) = webcam.read() 
+	gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY) 
+	faces = face_cascade.detectMultiScale(gray, 1.3, 4) 
+	for (x, y, w, h) in faces: 
+		cv2.rectangle(im, (x, y), (x + w, y + h), (255, 0, 0), 2) 
+		face = gray[y:y + h, x:x + w] 
+		face_resize = cv2.resize(face, (width, height)) 
+		#cv2.imwrite("PLEASE MOVE YOUR FACE")
+		cv2.imwrite('% s/% s.png' % (path, count), face_resize) 
+	count += 1
+	cv2.imshow('OpenCV', im)  
+	if cv2.waitKey(1) & 0xFF==ord('q'):
+		break
+
---
+++ face_recognize.py
@@ -0,0 +1,70 @@
+import cv2, sys, numpy, os
+#import urllib.request as ur
+import datetime
+#from notify_run import Notify
+haar_file = 'cascade.xml'
+datasets = 'datasets'
+array_to_store_names=[]
+
+print('INITIALISING') 
+
+(images, lables, names, id) = ([], [], {}, 0) 
+for (subdirs, dirs, files) in os.walk(datasets): 
+	for subdir in dirs: 
+		names[id] = subdir 
+		subjectpath = os.path.join(datasets, subdir) 
+		for filename in os.listdir(subjectpath): 
+			path = subjectpath + '/' + filename 
+			lable = id
+			images.append(cv2.imread(path, 0)) 
+			lables.append(int(lable)) 
+		id += 1
+(width, height) = (130, 100) 
+
+(images, lables) = [numpy.array(lis) for lis in [images, lables]] 
+
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.train(images, lables) 
+
+face_cascade = cv2.CascadeClassifier(haar_file) 
+webcam = cv2.VideoCapture(0) # ----- webcam
+#cap = cv2.VideoCapture('http://192.168.43.7:8080/video') #----- mobile_streaming
+
+date = datetime.datetime.now()
+
+sequencefile=str(date.day)+"\\"+str(date.month)+"\\"+str(date.year)
+store=sequencefile
+print(sequencefile)
+sequencefile+=".txt"
+f=open(sequencefile,"a+")
+f.write("%s "%(store))
+while True: 
+	(_, im) = webcam.read()
+	#(_, im) = cap.read() 
+	gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY) 
+	faces = face_cascade.detectMultiScale(gray, 1.3, 5) 
+	for (x, y, w, h) in faces: 
+
+		cv2.rectangle(im, (x, y), (x + w, y + h), (255, 0, 0), 2) 
+		face = gray[y:y + h, x:x + w] 
+		face_resize = cv2.resize(face, (width, height)) 
+		prediction = recognizer.predict(face_resize) 
+		cv2.rectangle(im, (x, y), (x + w, y + h), (255, 255, 0), 3)
+
+		if names[prediction[0]] not in array_to_store_names:
+			cv2.rectangle(im, (x, y), (x + w, y + h), (255, 255, 0), 3) 
+			cv2.putText(im, '% s' %(names[prediction[0]]), (x-10, y-10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0)) 
+			print("REGISTERED %s"%(names[prediction[0]]))
+			array_to_store_names.append(names[prediction[0]])
+			
+			f=open(sequencefile,"a+")
+			f.write("%s " %(names[prediction[0]]))
+			if cv2.waitKey(1) & 0xFF==ord('n'):
+				break
+
+	cv2.imshow('OpenCV', im) 
+	
+	if cv2.waitKey(1) & 0xFF==ord('q'): 
+		break
+f.write("\n")
